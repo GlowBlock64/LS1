b3855c79ded14ab7900125603a2f36c2224a86fa
Ondřej Chmelík <ondra.ch123@seznam.cz>
2024-12-12 12:25:36 +0100

    main: Uprava modelu.

--- models/variables.m
+++ models/variables.m
@@ -1,18 +1,18 @@
-%Parametry pro simulaci
+%% Parametry pro simulaci
 
 C2K = 273.15;
 
-T_0 = 23; %poc. teplota ve stupnich Celsia
-T_0 = T_0 + C2K;
+T0 = 23; %poc. teplota ve stupnich Celsia
+T0 = T0 + C2K;
 
 T_inf = 23; %teplota okolniho prostredu
 T_inf = T_inf + C2K;
 
 
-alph = 0.01; %skalovani inzenyrskych jednotek z procent na Watty dodavaneho vykonu
-alph2 = 0.0075; %druhe topeni ma mensi vykon 0.75W !
+alfa = 0.01; %skalovani inzenyrskych jednotek z procent na Watty dodavaneho vykonu
+alfa2 = 0.0075; %druhe topeni ma mensi vykon 0.75W !
 
-c_p = 500; %merna tepelna kapacita topneho telesa
+cp = 500; %merna tepelna kapacita topneho telesa
 A = 7e-4; %povrch topeni v m^2 - POZOR zmena oproti single-mass modelu, zmenseni ztrat vedenim do prostredi a naopak zvetseni predani tepla mezi telesy
 As = 5e-4; %povrch kontaktu mezi telesy
 
@@ -20,4 +20,15 @@ m = 4e-3; %hmotnost topneho telesa
 U = 10; %koeficient prestupu tepla
 eps = 0.5; %emisivita - snizeni z 0.9 od vyrobce, zmenseni ztrat vyzarovanim
 sig = 5.67e-8; %SB konstanta
+
+%% Definice koeficientů K
+
+K1 = (U*A)/(m*cp);
+K2 = (eps*sig*A)/(m*cp);
+K3 = (U*As)/(m*cp);
+K4 = (eps*sig*As)/(m*cp);
+Ku1 = alfa/(m*cp);
+Ku2 = alfa2/(m*cp);
+
+
 
